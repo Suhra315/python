76c5738817934a7cb1589697c334b8b45c54fc84
fathimath suhra <fathimasuhra315@gmail.com>
2025-09-03 14:01:44 +0530

    challenge task

---
+++ intro.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+print(f"Hi there! I'm {name}, a {age}-year-old who loves {hobby}. Nice to meet you!")
